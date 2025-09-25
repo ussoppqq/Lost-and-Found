4e65c263b77a5df4950d812071adcb33a2c2292c
sleeping-code <frangelicamuaya16@gmail.com>
2025-09-25 03:50:13 -0500

    add LostForm component and lost item report form

---
+++ laravel/app/Livewire/LostForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class LostForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.lost-form');
+    }
+}
