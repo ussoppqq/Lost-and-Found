7644b712d4412b751c481fe8753d4455d1aff1b6
sleeping-code <frangelicamuaya16@gmail.com>
2025-10-23 01:45:22 -0500

    fix: Correct timezone configuration and update anchor link to use URL helper

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -30,6 +30,7 @@ return [
     'locale' => env('APP_LOCALE', 'id'),
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
     'faker_locale' => env('APP_FAKER_LOCALE', 'id_ID'),
+    'timezone' => 'Asia/Jakarta',
 
     'cipher' => 'AES-256-CBC',
 
