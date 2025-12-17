6b47248107c5615230d83f9d4ee53a1bd0e7ee02
eljeyzydr <jeyzydrgithub@gmail.com>
2025-12-17 17:05:42 +0700

    fix: Correct column name in locations migration and reorder imports in UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use App\Models\User;
 use App\Models\Company;
 use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
